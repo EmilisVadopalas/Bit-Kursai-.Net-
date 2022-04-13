f62fe7c72e973b7d17d7c521cfa47e40d47182e0
Rokas Bundonis <RokasBundonis@gmail.com>
2022-04-13 20:30:44 +0300

    pirmos uzduotis

--- ManoPirmasDotNetProjektas/Program.cs
+++ ManoPirmasDotNetProjektas/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// 1 uzduotis: deklaruoti ir inicijuoti kintamuosius.
+// kintamieji:
+// myFirstString (string)
+// myFirstInt (int)
+// myFirstBool (bool)
+// myFirstDouble (double)
+// myFirstDecimal (decimal)
+// myFirstFloat (float)
+
+float myFirstFloat = 30.554f;
+
+// 2 uzduotis: sukurti string tipo kintamaji (naudoti var raktazodi) kurio reiksme '5000',
+// ta kintamaji konvertuoti i int tipa ir priskirti myFirstInt kintamajui
+
+var penkiTukstanciai = "5000";
