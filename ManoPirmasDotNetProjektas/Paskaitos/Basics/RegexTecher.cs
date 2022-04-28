3259f51437e968dd9ae6b08fe922fe34fdcdb058
Rokas Bundonis <RokasBundonis@gmail.com>
2022-04-28 19:04:06 +0300

    regex matcher

---
+++ ManoPirmasDotNetProjektas/Paskaitos/Basics/RegexTecher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ManoPirmasDotNetProjektas.Paskaitos
+{
+    public static class RegexTecher
+    {
+        public static void EnterAndCheckRegex()
+        {
+            Console.Write("Iveskite Regex: ");
+            var regexPattern = Console.ReadLine();
+            var regex = new Regex(regexPattern);
+
+            Console.WriteLine("Iveskite teksta kuri regex bandys matchinti");
+            Console.Write(": ");
+            var text = Console.ReadLine();
+
+            if (regex.IsMatch(text))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("tekstas atitinka regex'a");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("tekstas Neatitinka regex'o");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
--- ManoPirmasDotNetProjektas/Program.cs
+++ ManoPirmasDotNetProjektas/Program.cs
@@ -1,35 +1,6 @@
 ﻿using ManoPirmasDotNetProjektas.Paskaitos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Basic.PirmaUzduotisKetvirtosPaskaitos("tiesaiog tekstas kuri isprintis");
-
-Basic.PirmaUzduotisKetvirtosPaskaitos("daugiau teksto keturis kartus", 4);
-
-var skaicius = 5;
-
-System.Console.WriteLine(skaicius);
-Basic.TreciaUzduotisKetvirtosPaskaitos(ref skaicius);
-System.Console.WriteLine(skaicius);
-
-Basic.KetvirtaUzduotisKetvirtosPaskaitos(skaicius, out string daugiauMaziauLygu);
-System.Console.WriteLine($"{skaicius} yra {daugiauMaziauLygu} uz nuli");
-
-skaicius = 0;
-Basic.KetvirtaUzduotisKetvirtosPaskaitos(skaicius, out daugiauMaziauLygu);
-System.Console.WriteLine($"{skaicius} yra {daugiauMaziauLygu} uz nuli");
-
-skaicius = -20;
-Basic.KetvirtaUzduotisKetvirtosPaskaitos(skaicius, out daugiauMaziauLygu);
-System.Console.WriteLine($"{skaicius} yra {daugiauMaziauLygu} uz nuli");
-
-
-System.Console.WriteLine($"tuple: (true, \"stringas\", {skaicius})");
-var result = Basic.PenktaUzduotisKetvirtosPaskaitos(true, "stringas", skaicius);
-System.Console.WriteLine($"result.Item1 : {result.Item1}\n" +
-                            $"result.Item2 : {result.Item2}\n" +
-                            $"result.Item3 : {result.Item3}\n");
-
-System.Console.WriteLine($"skaiciu 1, 2, 3, 4, 5, 6, 7, 8, 9  ," +
-    $" suma: { Basic.SestaUzduotisKetvirtosPaskaitos(1, 2, 3, 4, 5, 6, 7, 8, 9)}");
-
-Basic.SeptintaUzduotisKetvirtosPaskaitos("arrow or in .NET called Expresion body type function");
-
+RegexTecher.EnterAndCheckRegex();
