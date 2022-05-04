6ee7d1dedae30e711dd0d28978c8ea9e9fc08fd0
Rokas Bundonis <RokasBundonis@gmail.com>
2022-05-04 21:07:32 +0300

    minor

---
+++ ManoPirmasDotNetProjektas/Paskaitos/OPP/Vehicle/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ManoPirmasDotNetProjektas.Paskaitos.OPP.Enums;
+
+namespace ManoPirmasDotNetProjektas.Paskaitos.OPP.Vehicle
+{
+    public class Vehicle
+    {
+        public double TopSpeed { get; set; }
+        public double Weight { get; set; }
+        public DateTime Year { get; init; }
+        public int MaxDistance { get; set; }
+        public VehicleType Type { get; set; }
+
+
+        public Vehicle() { }
+
+        public Vehicle(double speed, double weight, DateTime year, int distance, VehicleType type)
+        {
+            TopSpeed = speed;
+            Weight = weight;
+            Year = year;
+            MaxDistance = distance;
+            Type = type;
+        }
+    }
+}
