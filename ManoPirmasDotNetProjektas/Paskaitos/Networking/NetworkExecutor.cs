4ad527ff354b31315ce744a770f7530b5476886e
Rokas Bundonis <RokasBundonis@gmail.com>
2022-06-11 11:24:39 +0300

    lrytas scrape

--- ManoPirmasDotNetProjektas/Paskaitos/Networking/NetworkExecutor.cs
+++ ManoPirmasDotNetProjektas/Paskaitos/Networking/NetworkExecutor.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack.CssSelectors.NetCore;
+using HtmlAgilityPack;
+using PuppeteerSharp;
 
 namespace ManoPirmasDotNetProjektas.Paskaitos.Networking
 {
@@ -15,7 +18,14 @@ namespace ManoPirmasDotNetProjektas.Paskaitos.Networking
         private readonly ILoggerServise _logger;
 
         private string _mindaugasBlogUrl = "https://blog.mindaugas.cf";
-        private string _DadJokeUrl = "https://icanhazdadjoke.com/";
+        private string _dadJokeUrl = "https://icanhazdadjoke.com/";
+
+        private string _topoCentrasProcesorsBaseUrl = @"https://www.topocentras.lt/kompiuteriai-ir-plansetes/kompiuteriu-komponentai/procesoriai.html";
+        private string _topoCentrasLimit = @"?limit=80";
+        private string _topoCentrasPage = @"&p=1";
+
+        private string _LrytasBaseUrl = @"https://www.lrytas.lt/naujausi";
+        private string _LrytasPaginationParameter = @"?page=";
 
         public NetworkExecutor(ILoggerServise logger)
         {
@@ -26,7 +36,13 @@ namespace ManoPirmasDotNetProjektas.Paskaitos.Networking
         {
             //await GetWebHeader();
             //await GetDadJoke();
-            await Test10Jokes();
+            //await Test10Jokes();
+            //await ScrapeSomething();
+            await ScarpeLRytasHeadlines();
+
+
+            //await ScrapeTopoProcesors();
+            //await sodamkaoskijmfaspo();
         }
 
         private async Task GetWebHeader()
@@ -56,9 +72,9 @@ namespace ManoPirmasDotNetProjektas.Paskaitos.Networking
 
             try
             {
-                var result = await client.GetAsync(string.IsNullOrEmpty(id) 
-                    ? _DadJokeUrl 
-                    : $"{_DadJokeUrl}j/{id}");
+                var result = await client.GetAsync(string.IsNullOrEmpty(id)
+                    ? _dadJokeUrl
+                    : $"{_dadJokeUrl}j/{id}");
 
                 result.EnsureSuccessStatusCode();
 
@@ -66,7 +82,7 @@ namespace ManoPirmasDotNetProjektas.Paskaitos.Networking
             }
             catch (Exception ex)
             {
-                await _logger.LogWarning($"Cannot reach {_DadJokeUrl}");
+                await _logger.LogWarning($"Cannot reach {_dadJokeUrl}");
                 await _logger.LogWarning($"error: {ex.Message} \nTrace: {ex.StackTrace}");
 
                 return null;
@@ -91,7 +107,7 @@ namespace ManoPirmasDotNetProjektas.Paskaitos.Networking
                 }
                 while (userAwnser != "n" && userAwnser != "y");
 
-                if(userAwnser == "y")
+                if (userAwnser == "y")
                 {
                     Console.WriteLine("\nGreat!");
                     likedJokesIds.Add(joke.Id);
@@ -102,10 +118,124 @@ namespace ManoPirmasDotNetProjektas.Paskaitos.Networking
                 }
             }
 
-            foreach(var jokeId in likedJokesIds)
+            foreach (var jokeId in likedJokesIds)
             {
                 File.AppendAllText(@"./Juokingi.txt", $"{(await GetDadJoke(jokeId)).Joke}\n");
             }
+        }
+
+        private async Task ScrapeSomething()
+        {
+            var selector = "div > header > h2 > a:nth-child(1)";
+            var res = await Scrape("https://www.lrytas.lt/", selector);
+
+            Console.WriteLine(res);
+        }
+
+        private async Task<string> Scrape(string url, string cssSelector)
+        {
+            using var client = new HttpClient();
+
+            var result = await client.GetAsync(url);
+            var body = await result.Content.ReadAsStringAsync();
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(body);
+
+            var elements = htmlDocument.QuerySelectorAll(cssSelector);
+
+            StringBuilder sb = new();
+
+            foreach (var element in elements)
+            {
+                sb.Append(element.InnerText.Trim() + "\n");
+            }
+
+            return sb.ToString();
+        }
+
+       
+
+        private async Task<string> ScrapePage(string url)
+        {
+            using var client = new HttpClient();
+
+            var result = await client.GetAsync(url);
+
+            return await result.Content.ReadAsStringAsync();
+        }
+
+        private async Task ScarpeLRytasHeadlines()
+        {
+            var page = 1;
+            var headlines = new List<string>();
+
+            while(headlines.Count() < 200)
+            {
+                var pageContent = await ScrapePage($"{_LrytasBaseUrl}{_LrytasPaginationParameter}{page}");
+                page++;
+
+                headlines.AddRange(getHeadlines(pageContent));
+            }
+
+            var counter = 1;
+            foreach(var headline in headlines)
+            {
+                if (counter == 1)
+                {
+                    await File.WriteAllTextAsync("Lrytas_Straipsniu_antrastes.txt", $"{counter}: {headline}");
+                }
+                else
+                {
+                    await File.AppendAllTextAsync("Lrytas_Straipsniu_antrastes.txt", $"{counter}: {headline}");
+                }
+
+                counter++;
+            }
+            
+        }
+
+        private IEnumerable<string> getHeadlines(string htmlContent)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlContent);
+
+            var elements = htmlDocument.QuerySelectorAll("h2 .LPostContent__anchor");
+
+            var headlines = new List<string>();
+
+            foreach (var element in elements)
+            {
+                headlines.Add(element.InnerText);
+            }
+
+            return headlines;
+        }
+
+        private async Task ScrapeTopoProcesors()
+        {
+            var basePage = await ScrapePage($@"");
+            await File.WriteAllTextAsync("TopoCentras.html", basePage);
+        }
+
+        private async Task sodamkaoskijmfaspo()
+        {
+            await new BrowserFetcher(Product.Chrome).DownloadAsync();
+            using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                Product = Product.Chrome
+            });
+            using var page = await browser.NewPageAsync();
+            await page.GoToAsync(_topoCentrasProcesorsBaseUrl + _topoCentrasLimit + _topoCentrasPage);
+
+            var content = await page.GetContentAsync();
+
+
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(_topoCentrasProcesorsBaseUrl+ _topoCentrasLimit + _topoCentrasPage);
+            var somethibn = doc.DocumentNode.InnerHtml.ToString();
         }
     }
 }
